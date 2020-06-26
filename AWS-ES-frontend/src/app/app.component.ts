3f3ed93eefadc699e675db5ba2b8d3613bc8d559
oshadhaAK <oshadhakatulanda.16@cse.mrt.ac.lk>
2020-06-27 00:26:37 +0530

    data binding done

--- AWS-ES-frontend/src/app/app.component.ts
+++ AWS-ES-frontend/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AWS-ES-Song_Search';
+export class AppComponent implements OnInit {
+
+  lyrics: any;
+  name: any;
+ 
+
+  constructor() {
+    
+  }
+
+
+
+
+
+
+  ngOnInit() {
+    
+
+  }
+
+  searchLyrics(entry){
+    this.lyrics = entry
+    console.log(this.lyrics)
+  }
+
+  searchSong(entry){
+    this.name = entry
+    console.log(this.name)
+  }
+
+  onSearchChange(searchValue){
+    console.log(searchValue)
+  }
 }
+
