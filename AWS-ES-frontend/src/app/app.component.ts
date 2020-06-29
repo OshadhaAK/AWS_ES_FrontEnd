5d1d91e66b35df37df6acbacaf623db383f1ef87
oshadhaAK <oshadhakatulanda.16@cse.mrt.ac.lk>
2020-06-29 08:43:03 +0530

    dataservice added

--- AWS-ES-frontend/src/app/app.component.ts
+++ AWS-ES-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { SearchService } from '../app/search.service'
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,33 +10,39 @@ export class AppComponent implements OnInit {
 
   lyrics: any;
   name: any;
- 
+  isConnected = false;
+  status: string;
 
-  constructor() {
-    
+  constructor(private searchService: SearchService, private cd: ChangeDetectorRef) {
+    this.isConnected = false;
   }
-
-
-
-
 
 
   ngOnInit() {
-    
+    this.searchService.isAvailable().then(() => {
+      this.status = 'OK';
+      this.isConnected = true;
+    }, error => {
+      this.status = 'ERROR';
+      this.isConnected = false;
+      console.error('Server is down', error);
+    }).then(() => {
+      this.cd.detectChanges();
+    });
 
   }
 
-  searchLyrics(entry){
+  searchLyrics(entry) {
     this.lyrics = entry
     console.log(this.lyrics)
   }
 
-  searchSong(entry){
+  searchSong(entry) {
     this.name = entry
     console.log(this.name)
   }
 
-  onSearchChange(searchValue){
+  onSearchChange(searchValue) {
     console.log(searchValue)
   }
 }
---
+++ AWS-ES-frontend/src/app/search.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import * as elasticsearch from 'elasticsearch-browser';
+import { from } from 'rxjs';
+import { Client } from 'elasticsearch-browser';
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  private client: Client;
+
+  constructor() { 
+    if (!this.client) {
+      this._connect();
+    }
+  }
+
+  private connect() {
+    this.client = new Client({
+      host: 'http://localhost:9200',
+      log: 'trace'
+    });
+  }
+  private _connect() {
+    this.client = new elasticsearch.Client({
+      host: 'localhost:9200',
+      log: 'trace'
+    });
+  }
+  isAvailable(): any {
+    return this.client.ping({
+      requestTimeout: Infinity,
+      body: 'hello grokonez!'
+    });
+  }
+  
+  
+}
+
